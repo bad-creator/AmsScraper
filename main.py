fa4cb6743aaaa2d49cc739c03777cba24d883df5
sergheipascal <syryoga1@gmail.com>
2023-05-28 14:49:14 +0200

    Проводится по всем ссылкам AMS

--- main.py
+++ main.py
@@ -20,16 +20,29 @@ import re
 
 driver = webdriver.Chrome('venv/chromedriver_mac64/chromedriver')
 
-driver.get("https://jobs.ams.at/public/emps/jobs?page=1&query=Software-EntwicklerIn&location=wien&JOB_OFFER_TYPE=SB_WKO&JOB_OFFER_TYPE=IJ&JOB_OFFER_TYPE=BA&PERIOD=ALL&sortField=PERIOD")
+driver.get(
+    "https://jobs.ams.at/public/emps/jobs?page=1&query=Software-EntwicklerIn&location=wien&JOB_OFFER_TYPE=SB_WKO&JOB_OFFER_TYPE=IJ&JOB_OFFER_TYPE=BA&PERIOD=ALL&sortField=PERIOD")
 
 # Проверка наличия всплывающего окна
 popup_present = EC.presence_of_element_located((By.XPATH, '/html/body/sn-root/sn-cookie-banner/div/div/div'))
 
-time.sleep(5)
+time.sleep(3)
 # popup_decline_button = driver.find_element(By.CSS_SELECTOR, ".eom-button-row button:nth-child(1)")
 
-popup_decline_button = driver.find_element(By.XPATH, '/html/body/sn-root/sn-cookie-banner/div/div/div/div[2]/div/div[2]/button')
+popup_decline_button = driver.find_element(By.XPATH,
+                                           '/html/body/sn-root/sn-cookie-banner/div/div/div/div[2]/div/div[2]/button')
 
 popup_decline_button.click()
 
-time.sleep(15)
+# Получаю список вакансий
+vacancies_elements = driver.find_elements(By.CSS_SELECTOR, "#ams-search-result-list a[role='cell']:not([target='_blank'])")
+
+# Получаю список ссылок на вакансии
+vacancy_links = [vacancy.get_attribute('href') for vacancy in vacancies_elements]
+
+for link in vacancy_links:
+    time.sleep(1)
+    driver.get(link)  # Переходим на страницу конкретной вакансии
+    time.sleep(1)
+    driver.back()  # Переходим назад на исходной вкладке
+    time.sleep(1)
